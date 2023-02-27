dfcf7ba52481ecddfe004bcbc40b8e8d6b0dbb96
Dhillips19 <daniel.phillips@outlook.ie>
2023-02-27 20:04:36 +0000

    Changes to BugZap2

--- java/src/ie/tudublin/BugZap2.java
+++ java/src/ie/tudublin/BugZap2.java
@@ -6,7 +6,7 @@ public class BugZap2 extends PApplet
 {
     public void settings()
     {
-        size(1000, 1000);
+        size(800, 640);
     }
 
     public void setup()
@@ -16,16 +16,20 @@ public class BugZap2 extends PApplet
     
     float playerX, playerY;
     float playerWidth;
+    float halfPlayerWidth = playerWidth / 2;
     
     public void draw()
     {
         background(0);
+        drawPlayer(playerX, playerY, playerWidth);
     }
 
     void drawPlayer(float x, float y, float w)
     {
         // Draw player
         stroke(255);
+        float playerHeight = w / 2;
+        line(x - halfPlayerWidth , y + playerHeight, x + halfPlayerWidth, y + playerHeight);
 
     }
 
